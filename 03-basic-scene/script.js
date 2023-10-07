132d3cd503b6dbd8ccd089c9b4387c057b2c57f2
matt-manuel <matt-the-ogre@users.noreply.github.com>
2023-10-07 06:52:14 -0700

    finished lesson 03

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,4 +1,6 @@
 // console.log(THREE)
+
+// Scene
 const scene = new THREE.Scene()
 
 // create a cube and make it red
@@ -11,6 +13,23 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const fieldOfView = 75
+const aspectRatio = window.innerWidth / window.innerHeight
+// const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio)
 
+// move the camera back so we can see the cube
+camera.position.z = 3
+// camera.position.x = 1
+// camera.position.y = 1
 scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('canvas.webgl')
+// console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+renderer.render(scene, camera)
