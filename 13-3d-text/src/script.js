f9e37fac5c5eda27b2b183c2fad04ebefd35b080
matt-manuel <matt-the-ogre@users.noreply.github.com>
2023-10-10 13:59:05 -0700

    finished lesson 13

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 THREE.ColorManagement.enabled = false
 
@@ -16,10 +18,84 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Axes Helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
+
+// Fonts
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        // console.log('font loaded')
+        const myBevelSize = 0.02
+        const myBevelThickness = 0.03
+        const textGeometry = new TextGeometry(
+            'Lorem ipsum',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.1,
+                curveSegments: 50,
+                bevelEnabled: true,
+                bevelThickness: myBevelThickness,
+                bevelSize: myBevelSize,
+                bevelOffset: 0,
+                bevelSegments: 40
+            }
+        )
+        // textGeometry.computeBoundingBox()
+        // console.log(textGeometry.boundingBox)
+        // center the text
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - myBevelSize) / 2,
+        //     - (textGeometry.boundingBox.max.y - myBevelSize) / 2,
+        //     - (textGeometry.boundingBox.max.z - myBevelThickness) / 2
+        // )
+        textGeometry.center()
+        // textGeometry.computeBoundingBox()
+        // console.log(textGeometry.boundingBox)
+        // Physical material
+        /*
+        const textMaterial = new THREE.MeshPhysicalMaterial()
+        textMaterial.color = new THREE.Color(0xff0000)
+        textMaterial.metalness = 0.3
+        textMaterial.roughness = 0.4
+        */ 
+        // matcap material
+        const textMaterial = new THREE.MeshMatcapMaterial( {matcap: matcapTexture} )
+        
+        // textMaterial.wireframe = true
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
+
+        console.time('donuts')
+
+        // create 100 donuts with the same geometry and material in random positions up to 10 units away from the center and random sizes between 0.5 and 1.5 and random rotations
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        const donutMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+        for (let i = 0; i < 100; i++) {
+            const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+            scene.add(donut)
+        }
+        console.timeEnd('donuts')
+    }
+);
+
 
 /**
  * Object
@@ -29,7 +105,14 @@ const cube = new THREE.Mesh(
     new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+// scene.add(cube)
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.set(2, 2, 0)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -82,6 +165,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let lastTickTime = clock.getElapsedTime()
+let thisTickTime = lastTickTime
+
+// FPS Variables
+let lastTime = performance.now();
+let frameCount = 0;
 
 const tick = () =>
 {
@@ -90,8 +179,34 @@ const tick = () =>
     // Update controls
     controls.update()
 
+    // rotate the donuts slowly in two dimensions
+    let deltaTickTime = elapsedTime - lastTickTime
+    scene.traverse((child) => {
+        if (child instanceof THREE.Mesh && child.name !== 'text') {
+            // console.log(child.name)
+            child.rotation.x += deltaTickTime * 0.1
+            child.rotation.y += deltaTickTime * 0.1
+        }
+    })
+    lastTickTime = elapsedTime
+
+    
     // Render
+    const renderStartTime = performance.now();
     renderer.render(scene, camera)
+    const renderEndTime = performance.now();
+    const lastRenderTime = renderEndTime - renderStartTime;
+
+    // Calculate FPS
+    const performanceTime = performance.now();
+    frameCount++;
+    // only update every second (1000 milliseconds)
+    const updatems = 500;
+    if (performanceTime >= lastTime + updatems) {
+        document.getElementById("fps").innerText = `${frameCount*(1000/updatems)} FPS ${(1000/lastRenderTime).toFixed(2)} fps ${lastRenderTime.toFixed(2)} ms\ntriangles: ${renderer.info.render.triangles}`;
+        frameCount = 0;
+        lastTime = performanceTime;
+    }
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
