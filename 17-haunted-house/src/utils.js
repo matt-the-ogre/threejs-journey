17c4f8fc53c40f634f592359decfbfb95e66ff1b
Matt Manuel <matt-the-ogre@users.noreply.github.com>
2023-10-14 13:05:34 -0700

    part way through lesson 17

---
+++ 17-haunted-house/src/utils.js
@@ -0,0 +1,17 @@
+// Import the dat.gui library
+import * as dat from 'dat.gui';
+
+// Create a function to set up the debug menu
+function setupDebugMenu(value1, value2, value3, value4) {
+    // Create a new GUI object
+    const gui = new dat.GUI();
+
+    // Add the four values to the GUI
+    gui.add({ value1 }, 'value1', -100, 100);
+    gui.add({ value2 }, 'value2', -100, 100);
+    gui.add({ value3 }, 'value3', -100, 100);
+    gui.add({ value4 }, 'value4', -100, 100);
+}
+
+// Call the function with your four number values
+setupDebugMenu(0, 0, 0, 0);
